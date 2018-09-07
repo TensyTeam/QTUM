149490553f24f6eb9413c97e17d6d6ed4db256ae
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2018-09-07 16:34:19 +0300

    little fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: './index.js',
   output: {
     library: 'TEST',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  
   module: {
   }
 };
